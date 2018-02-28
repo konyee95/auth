55943bde5e8133213c73fcdc5dbdd6f864aa2fcc
Kon Yee <konyee95@gmail.com>
2018-02-28 20:28:36 +0800

    Log user out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, TextInput } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount(){
         firebase.initializeApp({
             apiKey: 'AIzaSyCMZqPhqGfoIie4gMvSkdx8_IhtUEMaVHs',
@@ -14,13 +16,42 @@ class App extends Component {
             storageBucket: 'authentication-68985.appspot.com',
             messagingSenderId: '924081389928'
           });
+
+          
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                this.setState({ loggedIn: true });
+            } else{
+                this.setState({ loggedIn: false });
+            }
+        });
     }
+
+    renderContent(){
+        switch (this.state.loggedIn){
+            case true:
+                return(
+                    <CardSection>
+                        <Button onPress={()=>firebase.auth().signOut()}>
+                        Log Out
+                        </Button>
+                    </CardSection>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large"/>;
+        }
+            
+    }
+        
+
 
     render(){
         return(
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -24,12 +24,12 @@ const styles = {
     },
     buttonStyle:{
         flex: 1,
-        alignSelf: 'stretch',
+        // alignSelf: 'stretch',
         borderRadius: 5,
         borderWidth: 1,
         borderColor: '#007aff',
         marginLeft: 5,
-        marginRight: 5
+        marginRight: 5,
     }
 }
 
